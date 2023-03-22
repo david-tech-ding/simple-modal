fa349d8cacfc30810e75ce281f338b6e73394565
David Ding <dingtechxing16@gmail.com>
2023-03-22 23:36:02 +0000

    Added overlay effect

--- script.js
+++ script.js
@@ -5,10 +5,12 @@ const closeModalBtn = document.querySelector('#close-modal-btn')
 
 openModalBtn.addEventListener('click', () => {
     modal.classList.add('open')
+    overlay.classList.add('show')
 })
 
 closeModalBtn.addEventListener('click', () => {
     modal.classList.remove('open')
+    overlay.classList.remove('show')
 })
 
 
