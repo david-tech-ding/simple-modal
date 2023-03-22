b7daa341efe7cbe4c674692a02cd5f29c5fbeab6
David Ding <dingtechxing16@gmail.com>
2023-03-22 23:30:12 +0000

    Selected elements by ID

---
+++ script.js
@@ -0,0 +1,7 @@
+const openModalBtn = document.querySelector('#open-modal-btn')
+const overlay = document.querySelector('#overlay')
+const modal = document.querySelector('modal')
+const closeModalBtn = document.querySelector('#close-modal-btn')
+
+
+
