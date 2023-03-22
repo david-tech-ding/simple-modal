fab5f8239b57258b698eef6a8281b33c3adcf273
David Ding <dingtechxing16@gmail.com>
2023-03-22 23:33:49 +0000

    Added open and close button onclick

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 const openModalBtn = document.querySelector('#open-modal-btn')
 const overlay = document.querySelector('#overlay')
-const modal = document.querySelector('modal')
+const modal = document.querySelector('#modal')
 const closeModalBtn = document.querySelector('#close-modal-btn')
+
+openModalBtn.addEventListener('click', () => {
+    modal.classList.add('open')
+})
+
+closeModalBtn.addEventListener('click', () => {
+    modal.classList.remove('open')
+})
 
 
 
